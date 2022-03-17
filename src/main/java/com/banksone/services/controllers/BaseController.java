cf52276f2f30203900720ce5d5224dc3008f00e4
banksone <13190202+banksone@users.noreply.github.com>
2022-03-17 14:50:42 +0100

    new endpoints

--- src/main/java/com/banksone/services/controllers/BaseController.java
+++ src/main/java/com/banksone/services/controllers/BaseController.java
@@ -1,6 +1,8 @@
 package com.banksone.services.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -17,6 +19,20 @@ public class BaseController {
 
     @GetMapping("/moviesapi/preferences")
     HashMap<String, String> getPreferences() {
+        HashMap jsonMap = new HashMap<String, String>();
+        jsonMap.put("text", "prefs");
+        return jsonMap;
+    }
+
+    @GetMapping("/moviesapi/details/{id}")
+    HashMap<String, String> getDetails(@PathVariable long id) {
+        HashMap jsonMap = new HashMap<String, String>();
+        jsonMap.put("text", "prefs");
+        return jsonMap;
+    }
+
+    @PostMapping("/moviesapi/like")
+    HashMap<String, String> giveLike(HashMap<String, String> like) {
         HashMap jsonMap = new HashMap<String, String>();
         jsonMap.put("text", "prefs");
         return jsonMap;
